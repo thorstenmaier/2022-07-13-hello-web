1d44905eaaced96d33de9a54793b317c69cefa30
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-07-13 10:56:22 +0200

    PathVariable

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	// http://localhost:8080/hello?name=Thorsten
-
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", required = false, defaultValue = "Governikus") String firstname) {
 		return "Hello " + firstname;
 	}
+
+	// http://localhost:8080/hello/Thorsten
+	@GetMapping("/hello/{name}")
+	public String helloPath(@PathVariable("name") String firstname) {
+		return "Hello " + firstname;
+	}
+
 }
